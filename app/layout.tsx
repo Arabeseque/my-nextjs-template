5a4053b7ea6be709b307ad51c26a6d024b9922fe
Arabeseque <hzy1210085279@gmail.com>
2023-08-19 19:43:36 +0800

    feat: add UI dependence

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,10 @@
 import './globals.css'
+import '@radix-ui/themes/styles.css';
+
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { Theme } from '@radix-ui/themes';
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,7 +20,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <Theme>
+          {children}
+        </Theme>
+        </body>
     </html>
   )
 }
